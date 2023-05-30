9e284c38b1eee489ea1481436929adf710f7e872
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-05-30 10:33:58 -0300

    Update App.js
    Adicionando a logica do botão remover

--- trilha-react-desafio-2-master/trilha-react-desafio-2-master/src/pages/App.js
+++ trilha-react-desafio-2-master/trilha-react-desafio-2-master/src/pages/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
@@ -9,43 +8,38 @@ import { api } from '../services/api';
 import { Container } from './styles';
 
 function App() {
-
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
+    const { data } = await api.get(`repos/${currentRepo}`);
 
-    const {data} = await api.get(`repos/${currentRepo}`)
-
-    if(data.id){
-
+    if (data.id) {
       const isExist = repos.find(repo => repo.id === data.id);
 
-      if(!isExist){
+      if (!isExist) {
         setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        setCurrentRepo('');
+        return;
       }
-
     }
-    alert('Repositório não encontrado')
-
-  }
+    alert('Repositório não encontrado');
+  };
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
-  }
-
+    const updatedRepos = repos.filter(repo => repo.id !== id);
+    setRepos(updatedRepos);
+  };
 
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
-      <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => (
+        <ItemRepo key={repo.id} handleRemoveRepo={handleRemoveRepo} repo={repo} />
+      ))}
     </Container>
   );
 }
